9956ad4a2538f58cbbeb06d58e15790edb49f884
JoseMorelVera <j.morel@duocuc.cl>
2026-07-06 20:57:29 -0400

    Refactor GestorDatos to GestorServicioTuristico
    Refactored GestorDatos class into GestorServicioTuristico and added method to generate a list of tourist services.

--- GestorDatos.java
+++ GestorDatos.java
@@ -9,34 +9,21 @@ import model.RutaGastronomica;
 import model.PaseoLacustre;
 import model.ExcursionCultural;
 
-public class GestorDatos {
-
-     public void ejecutarPrueba() {
+public class GestorServicioTuristico {
+    
+    public ArrayList<ServicioTuristico> generarServicioTuristico() {
         
-        // objetos de la clase Ruta gastronomica 
-        RutaGastronomica ruta1 = new RutaGastronomica("Ruta del vino", 4 , 3 );
-        RutaGastronomica ruta2 = new RutaGastronomica("Sabores locales", 6, 4 );
+        ArrayList<ServicioTuristico> servicioTuristico = new ArrayList<>();
         
-        System.out.println(ruta1);
-        System.out.println(ruta2);
-        System.out.println();
+        servicioTuristico.add(new RutaGastronomica("Degustacion de queso", 4 , 3));
+        servicioTuristico.add(new PaseoLacustre("Lago Manantiales", 4 , "Lancha"));
+        servicioTuristico.add(new ExcursionCultural("Visita museo", 3 , "Museo de ilustraciones"));
+        servicioTuristico.add(new RutaGastronomica("Fabrica de chocolate", 5 , 2));
+        servicioTuristico.add(new PaseoLacustre("Paseo Monte Isabella", 5 , "caminata"));
+        servicioTuristico.add(new ExcursionCultural("Recorrido historico", 6 , "Ruinas indigenas"));
         
-        // objetos de la clase paseo lacustre 
-        PaseoLacustre paseo1 = new PaseoLacustre("Visita volcan osorno", 3 , "Movilizacion local");
-        PaseoLacustre paseo2 = new PaseoLacustre("Tour por el lago", 5, "Lancha");
-        
-        System.out.println(paseo1);
-        System.out.println(paseo2);
-        System.out.println();
-        
-        // objetos de la clase excursion cultural
-        ExcursionCultural excursion1 = new ExcursionCultural("Colonos Alemanes", 6 , "Frutillar");
-        ExcursionCultural excursion2 = new ExcursionCultural("Historia Patrimonial", 5 , "Puerto Varas");
-        
-        System.out.println(excursion1);
-        System.out.println(excursion2);
-        System.out.println();
+        return servicioTuristico;
         
     }
-    
 }
+
